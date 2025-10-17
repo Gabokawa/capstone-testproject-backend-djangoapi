fd4e624738c7a37be65174559ed040f5002c0a8a
Joshua Gabriel Soriano <joshuagabriel011@gmail.com>
2025-10-18 02:23:52 +0800

    deleted unused lines

--- users/models.py
+++ users/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
-# class User(AbstractUser):
-#     username = models.CharField(max_length=150, unique=True)
-#     password = models.CharField(max_length=128)
-
-#     def __str__(self):
-#         return self.username
 
 class UserRole(models.Model):
     """User role definitions for RBAC"""
@@ -69,7 +61,12 @@ class UserRoleMapping(models.Model):
 class Address(models.Model):
     """User address management"""
     address_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='addresses', 
+        null=True
+    )
     region = models.CharField(max_length=100)
     full_address = models.TextField()
     latitude = models.DecimalField(max_digits=10, decimal_places=8)
