35fe5a41853a109f776a5697cdc1e4791e97cd54
Joshua Gabriel Soriano <joshuagabriel011@gmail.com>
2025-11-08 21:48:17 +0800

    added more details in quotes bookings serializer

--- quotes/serializer.py
+++ quotes/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Quote, Booking, ServiceIssue
+from requests.serializers import ServiceRequestDetailSerializer
 
 class QuoteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,14 +8,16 @@ class QuoteSerializer(serializers.ModelSerializer):
         fields = '__all__'  # This tells DRF to serialize *all* fields on the model
         read_only_fields = ['quote_id', 'created_at']
 
-class BookingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Booking
-        fields = '__all__'
-        read_only_fields = ['booking_id', 'created_at']
-
 class ServiceIssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceIssue
         fields = '__all__'
         read_only_fields = ['issue_id', 'reported_at']
+
+class BookingSerializer(serializers.ModelSerializer):
+    request = ServiceRequestDetailSerializer(read_only=True)
+    quote = QuoteSerializer(read_only=True)
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        read_only_fields = ['booking_id', 'created_at']
